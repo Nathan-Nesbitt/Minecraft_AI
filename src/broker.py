76d7b11e442c459972005057a84a3b83803281c5
Nathan Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2021-02-18 00:22:09 -0800

    Added multiple data objects for Minecraft_Store and Fixed File Writes
    I capped the buffer on the append file to request that it flushes after 1000 lines. This is not a huge change, but is better than having n*8000 possible lines in memory.
    
    I also fixed the Data and Minecraft_Store objects so they append properly so you have multiple files that can be written to at once. This fixes the renaming issue

--- src/broker.py
+++ src/broker.py
@@ -2,6 +2,7 @@ import json
 import asyncio
 import websockets
 from minecraft_store import Minecraft_Store
+
 """
     This is the broker file which handles communication between the client and the backend.
     It manages json objects send from the client side and redirects them to the appropriate libraries.
@@ -9,6 +10,7 @@ from minecraft_store import Minecraft_Store
     This file was developed by: Carlos Rueda Carrasco and Nathan Nesbitt
     Date: 30-01-2021
 """
+
 
 class Broker:
     def __init__(self):
@@ -18,22 +20,22 @@ class Broker:
 
     def establish_connection(self):
         """
-            Starts the connection through a specific port.
+        Starts the connection through a specific port.
         """
-        start_connection = websockets.serve(self.response, host='localhost', port=5678)
+        start_connection = websockets.serve(self.response, host="localhost", port=5678)
         asyncio.get_event_loop().run_until_complete(start_connection)
         asyncio.get_event_loop().run_forever()
 
     async def response(self, websocket, path):
         """
-            Listens in for the clients response and loads the json string into a dictionary format.
-            Takes the message into another method for handling while returning a message to the client.
+        Listens in for the clients response and loads the json string into a dictionary format.
+        Takes the message into another method for handling while returning a message to the client.
 
-            @param  websocket: An initialized websocket that handles messages being sent over
-            @param path: Path of the request
+        @param  websocket: An initialized websocket that handles messages being sent over
+        @param path: Path of the request
         """
         while True:
-            try: 
+            try:
                 message = await websocket.recv()
                 json_dict = json.loads(message)
                 status = self.targetProcessId(json_dict)
@@ -42,17 +44,17 @@ class Broker:
             except websockets.exceptions.ConnectionClosedOK:
                 print("Successfully closed!")
                 await websocket.close()
-                break 
+                break
 
     def targetProcessId(self, json_dictionary):
         """
-            Handles and checks within the dictionary where the game data needs to be sent, either for storage or for making ML models.
+        Handles and checks within the dictionary where the game data needs to be sent, either for storage or for making ML models.
 
-            @param json_dictionary: The json object that was converted into a dictionary
+        @param json_dictionary: The json object that was converted into a dictionary
         """
-        if (json_dictionary['header']['targetProcess']=='MinecraftStore'):
+        if json_dictionary["header"]["targetProcess"] == "MinecraftStore":
             return self.store(json_dictionary)
-        elif(json_dictionary['header']['targetProcess']=='MinecraftLearns'):
+        elif json_dictionary["header"]["targetProcess"] == "MinecraftLearns":
             return self.minecraft_learns(json_dictionary)
         else:
             print("Incorrect naming for targetProcess")
@@ -60,29 +62,33 @@ class Broker:
 
     def message_sent_back(self, json_dictionary, status):
         """
-            Handles the message that is being sent back to the front end so it knows if it was successfully handled or not. 
+        Handles the message that is being sent back to the front end so it knows if it was successfully handled or not.
 
-            @param json_dictionary: The json object that was converted into a dictionary
-            @param status: True or False depending if the action failed
+        @param json_dictionary: The json object that was converted into a dictionary
+        @param status: True or False depending if the action failed
         """
         message_for_client = {
-                                "header": {
-                                    "UUID": json_dictionary['header']['UUID'],
-                                    "status": str(status)
-                                }
-                            }
+            "header": {"UUID": json_dictionary["header"]["UUID"], "status": str(status)}
+        }
         message_for_client = json.dumps(message_for_client)
         return message_for_client
 
     def store(self, json_dictionary):
         """
-            Stores the game data into the clients local computer.
+        Stores the game data into the clients local computer.
 
-            @param json_dictionary: The json object that was converted into a dictionary
+        @param json_dictionary: The json object that was converted into a dictionary
         """
-        file_name = json_dictionary['header']['fileName']
-        #file_location = self.storage.rename(file_name)
-        file_location = self.storage.store_filesystem(json_dictionary['body']['data']['body'], file_name)
+        file_name = json_dictionary["header"]["fileName"]
+        UUID = json_dictionary["header"]["UUID"]
+        body = json_dictionary["body"]["data"]["body"]
+
+        # If the UUID hasn't been created, then we need to create a file
+        if not self.storage.files[UUID]:
+            self.storage.add_file(file_name, UUID)
+
+        # Adds the line to the file for this UUID
+        file_location = self.storage.store_filesystem(body, file_name, UUID)
         print(file_location)
         print("Sent to storage")
         return True
@@ -98,15 +104,15 @@ class Broker:
 
     def minecraft_learns(self, json_dictionary):
         """
-            Calls on the AI to produce ML models.
+        Calls on the AI to produce ML models.
 
-            @param json_dictionary: The json object that was converted into a dictionary
+        @param json_dictionary: The json object that was converted into a dictionary
         """
         try:
-            file_name = json_dictionary['header']['fileName']
-            model_type = json_dictionary['header']['model_type']
-            response_variable = json_dictionary['header']['response_variable']
-            function = json_dictionary['header']['function']
+            file_name = json_dictionary["header"]["fileName"]
+            model_type = json_dictionary["header"]["model_type"]
+            response_variable = json_dictionary["header"]["response_variable"]
+            function = json_dictionary["header"]["function"]
             print("Sent to Minecraft Learns")
             return True
         except Exception as e:
--- src/minecraft_store.py
+++ src/minecraft_store.py
@@ -1,7 +1,7 @@
 """
     This is a class that helps store data into a local location on the clients computer.
 
-    Written By: Carlos Rueda Carrasco
+    Written By: Carlos Rueda Carrasco and Nathan Nesbitt
     Date: 2021-01-08
 """
 # Import all the necessary packages
@@ -9,31 +9,45 @@ import json
 from datetime import datetime
 from data import Data
 
+
 class Minecraft_Store:
-    def __init__(self): 
-        self.client_data = Data()
+    def __init__(self):
+        # Files that can be written to
+        self.files = {}
 
-    def store_filesystem(self, data, name):
+    def store_filesystem(self, data, name, file):
         """
-            Method that takes the data from the client and then stores it into the local computer.
+        Method that takes the data from the client and then stores it into the local computer.
 
-            @param data: The data being stored, will be in a json dictionary format
+        @param data: The data being stored, will be in a json dictionary format
+
+        @param name:
+
+        @param file: Which file you are writing the data to, the UUID is normally this
         """
-        #if (self.client_data.already_made(name)==True):
+        # if (self.client_data.already_made(name)==True):
         #    self.client_data.add_observation(data)
-        #else:
-        self.client_data.save_observation(data)
-        #self.client_data.rename_file(name)
-        return str(self.client_data.absolute_path())
-    
-    def rename(self, name):
-        self.client_data.rename_file(name)
-    
+        # else:
+        self.files[file].save_observation(data)
+        # self.client_data.rename_file(name)
+        return str(self.files[file].absolute_path())
+
     def timestamp_format(self):
         """
-            Method that retrieves the time and date for our file naming
+        Method that retrieves the time and date for our file naming
         """
         time_sent_back = ""
         current = datetime.now().strftime("%Y-%m-%d")
         time_sent_back = time_sent_back + current
         return time_sent_back
+
+    def add_file(self, filename, UUID):
+        """
+        Adds a new file to the Minecraft_Store object based on the UUID
+        sent from the user. This allows for multiple files and multiple
+        event handlers to be run at the same time.
+
+        @param filename: Filename where the data should be saved.
+        @param UUID: Callback to the process that initialized the file.
+        """
+        self.files[UUID] = Data(filename=filename)
